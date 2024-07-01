39563643e9a751e1230cf2500c65588dce00df8e
akram1903 <akram.a.e.2002@gmail.com>
2024-07-01 20:41:53 +0300

    project organization: separating handlers from main file

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -55,138 +52,6 @@ func init() {
 	checkErr(err)
 
 	coll = client.Database(dbName).Collection(collectionName)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	err := rnd.Template(w, http.StatusOK, []string{"./static/home.tpl"}, nil)
-	checkErr(err)
-}
-
-func fetchTodos(w http.ResponseWriter, r *http.Request) {
-	var todos []todoModel
-
-	cursor, err := coll.Find(context.TODO(), bson.D{})
-	checkErr(err)
-
-	defer cursor.Close(context.TODO())
-	for cursor.Next(context.TODO()) {
-		var t todoModel
-		err := cursor.Decode(&t)
-		checkErr(err)
-		todos = append(todos, t)
-	}
-
-	todoList := []todo{}
-	for _, t := range todos {
-		todoList = append(todoList, todo{
-			ID:        t.ID.Hex(),
-			Title:     t.Title,
-			Completed: t.Completed,
-			CreatedAt: t.CreatedAt,
-		})
-	}
-
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"data": todoList,
-	})
-}
-
-func createTodo(w http.ResponseWriter, r *http.Request) {
-	var t todo
-
-	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-		rnd.JSON(w, http.StatusProcessing, err)
-		return
-	}
-
-	if t.Title == "" {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The title is required",
-		})
-		return
-	}
-
-	tm := todoModel{
-		ID:        primitive.NewObjectID(),
-		Title:     t.Title,
-		Completed: false,
-		CreatedAt: time.Now(),
-	}
-
-	_, err := coll.InsertOne(context.TODO(), tm)
-	checkErr(err)
-
-	rnd.JSON(w, http.StatusCreated, renderer.M{
-		"message": "todo created successfully",
-		"todo_id": tm.ID.Hex(),
-	})
-}
-
-func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimSpace(chi.URLParam(r, "id"))
-	objID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The id is invalid",
-		})
-		return
-	}
-
-	_, err = coll.DeleteOne(context.TODO(), bson.M{"_id": objID})
-	if err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "failed to delete todo",
-			"error":   err,
-		})
-		return
-	}
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"message": "todo deleted successfully",
-	})
-}
-
-func updateTodo(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimSpace(chi.URLParam(r, "id"))
-	objID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The id is invalid",
-		})
-		return
-	}
-
-	var t todo
-	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
-		rnd.JSON(w, http.StatusProcessing, err)
-		return
-	}
-
-	if t.Title == "" {
-		rnd.JSON(w, http.StatusBadRequest, renderer.M{
-			"message": "The title is required",
-		})
-		return
-	}
-
-	update := bson.M{
-		"$set": bson.M{
-			"title":     t.Title,
-			"completed": t.Completed,
-		},
-	}
-
-	_, err = coll.UpdateOne(context.TODO(), bson.M{"_id": objID}, update)
-	if err != nil {
-		rnd.JSON(w, http.StatusProcessing, renderer.M{
-			"message": "failed to update todo",
-			"error":   err,
-		})
-		return
-	}
-
-	rnd.JSON(w, http.StatusOK, renderer.M{
-		"message": "todo updated successfully",
-	})
 }
 
 func main() {
